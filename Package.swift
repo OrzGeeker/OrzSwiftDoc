7962ba07d033218777b669f9ac541f8894f42a08
wangzhizhou <wangzhizhou@bytedance.com>
2024-09-07 18:56:27 +0800

    Init Commit

---
+++ Package.swift
@@ -0,0 +1,27 @@
+// swift-tools-version: 6.0
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "OrzSwiftDoc",
+    products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
+        .library(
+            name: "OrzSwiftDoc",
+            targets: ["OrzSwiftDoc"]),
+    ],
+    dependencies: [
+      .package(url: "https://github.com/swiftlang/swift-docc-plugin.git", from: "1.4.2")  
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        .target(
+            name: "OrzSwiftDoc"),
+        .testTarget(
+            name: "OrzSwiftDocTests",
+            dependencies: ["OrzSwiftDoc"]
+        ),
+    ]
+)
---
+++ Sources/OrzSwiftDoc/OrzSwiftDoc.swift
@@ -0,0 +1,3 @@
+// The Swift Programming Language
+// https://docs.swift.org/swift-book
+// https://swiftlang.github.io/swift-docc-plugin/documentation/swiftdoccplugin/
